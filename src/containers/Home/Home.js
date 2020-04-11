cc0d691b02b221622631b15bb5c4a4db803db962
dastasoft <dastasoft@gmail.com>
2020-04-11 22:22:36 +0200

    home into form to catch enter

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -10,16 +10,25 @@ const Home = ({ onRoomName }) => {
 
   const inputChangeHandler = ev => setRoomName(ev.target.value);
 
+  const onSubmitHandler = ev => {
+    ev.preventDefault();
+    if (roomName.length > 0) {
+      onRoomName(roomName);
+    }
+  };
+
   return (
     <HomeLayout>
       <img src={logo} alt="Logo GrowMeeting" />
-      <Input
-        type="text"
-        placeholder="Join or Create Room"
-        value={roomName}
-        onChange={inputChangeHandler}
-      />
-      <Button onClick={() => onRoomName(roomName)}>SEND</Button>
+      <form onSubmit={onSubmitHandler}>
+        <Input
+          type="text"
+          placeholder="Join or Create Room"
+          value={roomName}
+          onChange={inputChangeHandler}
+        />
+        <Button>SEND</Button>
+      </form>
     </HomeLayout>
   );
 };
@@ -30,6 +39,14 @@ const HomeLayout = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  form {
+    display: inherit;
+    flex-direction: inherit;
+    justify-content: inherit;
+    align-items: inherit;
+  }
+
   input {
     text-align: center;
     font-size: 25px;
