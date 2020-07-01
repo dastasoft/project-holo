e6097746d2022a309b00c08ff2e8357aa9de1b3a
dastasoft <dastasoft@gmail.com>
2020-07-02 00:20:59 +0200

    first release

--- src/electron.js
+++ src/electron.js
@@ -1,29 +1,35 @@
-const electron = require('electron');
+const { app, BrowserWindow, screen } = require('electron');
 const isDev = require('electron-is-dev');
 const path = require('path');
 
-const { app, BrowserWindow } = electron;
-
-let mainWindow;
-
 function createWindow() {
-  mainWindow = new BrowserWindow({
+  // Create the browser window.
+  // const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  const win = new BrowserWindow({
     width: 900,
-    height: 680
+    height: 680,
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
 
-  mainWindow.loadURL(
-    isDev
-      ? 'http://localhost:1234'
-      : `file://${path.join(__dirname, '../dist/index.html')}`
+  // Load the URL
+  win.loadURL(
+    isDev ? 'http://localhost:1234' : 'https://grow-meeting.netlify.app/'
   );
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+
+  // Open the DevTools in DEV
+  if (isDev) win.webContents.openDevTools();
 }
 
-app.on('ready', createWindow);
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
 
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -31,7 +37,12 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  if (mainWindow === null) {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
